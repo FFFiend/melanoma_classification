440f9f75e0f22eaf1a77c808d5497e55046419c9
FFFiend <owais.zahid@mail.utoronto.ca>
2023-12-23 02:08:21 -0500

    completed final model

--- model.py
+++ model.py
@@ -5,65 +5,75 @@ import torch
 import torch.nn as nn
 
 class MelanomaCNN(nn.Module):
-    def __init__(self, num_channels, normalize_batch=True):
+    def __init__(self, width=3, normalize_batch=True):
         """
         Init method for the model.
         """
         super(MelanomaCNN, self).__init__()
-
-        self.num_channels = num_channels
-        self.normalize_batch = normalize_batch
-
-        # TODO: incomplete architecture, 
-        # FINAL DELIVERABLE complete on Dec 24th post exams.
-
-        self.conv1 = nn.Convd2d(
-            in_channels=3,
-            out_channels=self.num_channels,
-            kernel_size=9,
+        self.width = width
+        self.bn = normalize_batch
+        self.maxpool_layer = nn.MaxPool2d(2,2)
+        # first conv2d layer
+        self.conv1 = nn.Conv2d(
+            in_channels=self.width,
+            out_channels=self.width,
+            kernel_size=3,
         )
 
         self.conv2 = nn.Conv2d(
-            in_channels=self.num_channels * 2,
-            out_channels = None,
-            kernel_size = 9
-        )
-
-        self.maxpool1 = nn.MaxPool2d(
+            in_channels=self.width,
+            out_channels=32,
             kernel_size=3
         )
 
         self.conv3 = nn.Conv2d(
-            in_channels = 256
+            in_channels=32,
+            out_channels=64,
+            kernel_size=3
         )
 
-        if normalize_batch:
-            self.bn_1 = nn.BatchNorm2d(self.num_channels)
+        self.conv4 = nn.Conv2d(
+            in_channels=64,
+            out_channels=128,
+            kernel_size=3
+        )
+        # 3 batch norm channels corresponding to output num channels of each conv
+        # layer.
+        if self.bn:
+          self.bn1 = nn.BatchNorm2d(self.width)
+          self.bn2 = nn.BatchNorm2d(32)
+          self.bn3 = nn.BatchNorm2d(64)
+          self.bn4 = nn.BatchNorm2d(128)
 
-        self.dropout_layer = nn.Dropout(p=0.4)
+        self.fc1 = nn.Linear(32768 ,256)
+        self.fc2 = nn.Linear(256,2)
 
-        self.fc1 = nn.Linear()
-        self.fc2 = nn.Linear()
+        # final output should be batch size * 2
 
+    def forward(self,x):
+        x = self.maxpool_layer(torch.relu(self.conv1(x)))
 
-    def forward(self, x):
-        # TODO x.resize()
-        x = self.maxpool1(torch.relu(self.conv1(x)))
-        if self.bn_1:
-            x = self.bn_1(x)
-        x = self.maxpool1(torch.relu(self.conv1(x)))
-        if self.bn_1:
-            x = self.bn_1(x)
-        x = self.maxpool1(torch.relu(self.conv2(x)))
-
-        x = self.dropout_layer(x)
-
-        if self.bn_1:
-            x = self.bn_1(x)
-        x = self.maxpool1(torch.relu(self.conv3(x)))
-        if self.bn_1:
-            x = self.bn_1(x)
+        if self.bn:
+            x = self.bn1(x)
         
-        x = x.view(-1, self.num_channels)
+        x = self.maxpool_layer(torch.relu(self.conv2(x)))
+
+        if self.bn:
+            x = self.bn2(x)
+        
+        x = self.maxpool_layer(torch.relu(self.conv3(x)))
+
+        if self.bn:
+            x = self.bn3(x)
+
+        x = self.maxpool_layer(torch.relu(self.conv4(x)))
+
+        if self.bn:
+          x = self.bn4(x)
+
+        # flatten the image.
+        x = x.view(x.shape[0],-1)
+
+        # might swap relu out for smth else
         x = torch.relu(self.fc1(x))
-        return x
+        return self.fc2(x)
