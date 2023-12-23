a7abd7f70639f3bc7c74faa9c041c4bca21203fb
FFFiend <owais.zahid@mail.utoronto.ca>
2023-12-23 02:28:06 -0500

    added accuracy function from colab

--- accuracy_metric.py
+++ accuracy_metric.py
@@ -1,3 +1,18 @@
-def accuracy(model_outputs, evaluation_data):
-    pass
+import torch
+def accuracy(model, data, device="cuda:0"):
+    """
+    Accuracy function, self explanatory.
+    """
+    loader = torch.utils.data.DataLoader(data, batch_size=32)
+    model.to(device)
+    model.eval() # annotate model for evaluation (important for batch normalization)
+    correct = 0
+    total = 0
+    for imgs, labels in loader:
+        labels = labels.to(device)
+        output = model(imgs.to(device))
+        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+        total += imgs.shape[0]
+    return correct / total
 
