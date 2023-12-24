1044c5c03f1c44fac394a5a22e796b9f3fffa60b
FFFiend <owais.zahid@mail.utoronto.ca>
2023-12-24 06:22:15 -0500

    truncated video img

--- host.py
+++ host.py
@@ -34,6 +34,11 @@ def _predict(*args):
     for row in args:
         img.append(row)
     img = np.array(img)
+    if (img.shape[0]*img.shape[1] > 300*300):
+        img = img[:301]
+        for i in range(len(img)):
+            img[i] = img[i][:300]
+            
     img = np.transpose(img, (2, 0, 1))
     prediction = MODEL(torch.from_numpy(img.astype(np.float32)).unsqueeze(0))
     prediction = prediction.max(1, keepdim=True)[1]
