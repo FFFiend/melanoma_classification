e683b118a610ee8f67c5db379e57ca7fde8983e2
FFFiend <owais.zahid@mail.utoronto.ca>
2023-12-24 06:31:56 -0500

    fixed model.py docstring

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 """
-IMPORTANT! Find site deployed at: 
+IMPORTANT! Find site deployed at: https://melanomas.streamlit.app
 Reference class for the CNN architecture used for the classifier.
 """
 import torch
